268af74f5b0a496ab797edc1e29024ad73c1ecbc
Holden Altaffer <haltaffer@gmail.com>
2018-03-28 06:34:39 +0000

    automatically backed up by learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,19 +1,5 @@
 require_relative "./vehicle.rb"
 
-class Vehicle
-  attr_accessor :wheel_size, :wheel_number
+class Car < Vehicle
 
-  def initialize(wheel_size, wheel_number)
-    @wheel_size = wheel_size
-    @wheel_number = wheel_number
-  end
-
-  def go
-    "vrrrrrrrooom!"
-  end
-
-  def fill_up_tank
-    "filling up!"
-  end
-  
 end
